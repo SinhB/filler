de9b54b0d472142bff4aa6a87d8c0b59283495f3
yannbecret <ybecret@student.42.fr>
2019-04-11 17:56:09 +0200

    adding print_piece fct

--- srcs/parse_piece.c
+++ srcs/parse_piece.c
@@ -6,11 +6,23 @@
 /*   By: yabecret <yabecret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 16:48:12 by yabecret          #+#    #+#             */
-/*   Updated: 2019/04/11 17:42:00 by yabecret         ###   ########.fr       */
+/*   Updated: 2019/04/11 17:54:30 by yabecret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+
+void	print_piece(t_filler *filler)
+{
+	int i;
+
+	i = 0;
+	while (i < filler->piece.height)
+	{
+		ft_dprintf(2, "%s\n", filler->piece.square[i]); //fd = 2 to print without affecting VM input -> only for testing
+		i++;
+	}
+}
 
 int	parse_piece(t_filler *filler)
 {
@@ -21,8 +33,6 @@ int	parse_piece(t_filler *filler)
 	if (!get_piece_size(filler))
 		return (errors(2));
 	init_piece(filler);
-//	ft_dprintf(2, "h : %d\n", filler->piece.height);
-//	ft_dprintf(2, "w : %d\n", filler->piece.width);
 	while (i < filler->piece.height)
 	{
 		get_next_line(0, &line);
@@ -32,6 +42,6 @@ int	parse_piece(t_filler *filler)
 		//ft_memdel((void**)&line);
 		i++;
 	}
-//	print_map(filler);
+	print_piece(filler);
 	return (1);
 }
